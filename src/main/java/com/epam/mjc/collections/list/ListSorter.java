1ece04be54a0f8418f67d33ec36eaf427a200714
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-07 20:48:58 +0500

    4 classes implemented

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -11,5 +11,25 @@ public class ListSorter {
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        // Parse strings to integers
+        int x = Integer.parseInt(a);
+        int y = Integer.parseInt(b);
+
+        // Calculate function values
+        int funcValueX = calculateFunction(x);
+        int funcValueY = calculateFunction(y);
+
+        // If function values are different, sort based on function values
+        if (funcValueX != funcValueY) {
+            return Integer.compare(funcValueX, funcValueY);
+        } else {
+            // If function values are equal, sort elements in ascending order
+            return Integer.compare(x, y);
+        }
+    }
+
+    // Function to calculate 5x^2 + 3
+    private int calculateFunction(int x) {
+        return 5 * x * x + 3;
     }
 }
